616ae2b89dcf9d6a67de921889b1d3c86117d564
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-04-15 23:49:19 +0200

    Script to send to influx v2

--- infuxlogger.sh
+++ infuxlogger.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+FILE="obras.log"
+
 BUCKET="testing"
 ORG="electronicayciencia@gmail.com"
 URL="https://eu-central-1-1.aws.cloud2.influxdata.com"
 TOKEN="l3-Ay...V7n44Gw=="
 
-FILE="obras.log"
-
-
 tail -f $FILE | while read time timestamp rms maxvalue
 do
-	lineformat="sndobras rms=$rms,maxvalue=$maxvalue ${timestamp}000000000"
+    lineformat="sndobras rms=$rms,maxvalue=$maxvalue ${timestamp}000000000"
 	
-	echo "$time => $lineformat"
+    echo "$time => $lineformat"
 
     curl -sS "$URL/api/v2/write?bucket=$BUCKET&org=$ORG" \
         -H "Authorization: Token $TOKEN" \
